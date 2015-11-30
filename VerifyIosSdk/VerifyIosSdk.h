c2be171aff1465e4fd0aa9792dc2c7a5e65b76f1
ErikPoort <erik@mediamonks.com>
2015-11-30 16:54:41 +0100

    - Slowly adding headers

--- VerifyIosSdk/VerifyIosSdk.h
+++ VerifyIosSdk/VerifyIosSdk.h
@@ -15,3 +15,4 @@ FOUNDATION_EXPORT double VerifyIosSdkVersionNumber;
 FOUNDATION_EXPORT const unsigned char VerifyIosSdkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <VerifyIosSdk/PublicHeader.h>
+#import <VerifyIosSdk/NexmoClient.swift>
